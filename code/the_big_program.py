49ce3ba545c20ad742cb687d97f86f5a594dba25
Erik Finnman <erik.finnman@layer10.se>
2023-09-11 15:18:53 +0200

    Initial commit

--- code/the_big_program.py
+++ code/the_big_program.py
@@ -2,4 +2,10 @@ class TheBigProgram:
     _city = "Budapest"
 
     def do_some_stuff(self) -> str:
-        return f"We're in {self._city}!"
+        output = f"We're in {self._city}!"
+        print(output)
+        return output
+
+
+if __name__ == "__main__":
+    TheBigProgram().do_some_stuff()
